voxpupuli/puppet-anubis

commit 57217c8b4957b98eaebef77123c874c7912b9f22

require package for the service, so it gets restarted on upgrade

manifests/instance.pp
@@ -49,7 +49,8 @@ define anubis::instance (
   $service_ensure = $ensure == 'present'
 
   service { "anubis@${title}":
-    ensure => $service_ensure,
-    enable => $service_ensure,
+    ensure  => $service_ensure,
+    enable  => $service_ensure,
+    require => Package['anubis'],
   }
 }

commit 098698bff00fd4322a2add1eeeabfc95bd85ecb8

subscribe, not require, the anubis package
otherwise the ordering is right, but no restart happens

Fixes: 57217c8b4957b98eaebef77123c874c7912b9f22

manifests/instance.pp
@@ -49,8 +49,8 @@ define anubis::instance (
   $service_ensure = $ensure == 'present'
 
   service { "anubis@${title}":
-    ensure  => $service_ensure,
-    enable  => $service_ensure,
-    require => Package['anubis'],
+    ensure    => $service_ensure,
+    enable    => $service_ensure,
+    subscribe => Package['anubis'],
   }
 }